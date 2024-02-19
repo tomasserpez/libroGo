be021238387a3a50bf1dfa6f8cc8b532be953dcf
Tomás Serpez <tomasserpez@gmail.com>
2024-02-19 12:25:38 -0300

    Interprete masomenos terminado, quedan pendientes...

--- object/object.go
+++ object/object.go
@@ -3,7 +3,10 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
+	"tomlang/ast"
 )
 
 type ObjectType string
@@ -14,6 +17,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Object interface {
@@ -53,3 +57,28 @@ type Error struct {
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
--- token/token.go
+++ token/token.go
@@ -4,63 +4,65 @@ package token
 type TokenType string
 
 var keywords = map[string]TokenType{
-	"fn":		FUNCTION,
-	"let":		LET,
-	"true":		TRUE,
-	"false":	FALSE,
-	"if": 		IF,
-	"else":		ELSE,
-	"return":	RETURN,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
-func LookupIdent(ident string) TokenType{
-	if tok, ok := keywords[ident];ok{
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
 		return tok
 	}
 	return IDENT
 }
 
 const (
-	ILLEGAL		= "ILLEGAL"
-	EOF			= "EOF"
-	
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
 	//Identificadores + Literales
-	IDENT		= "IDENT"	//add, foobar, x, y...
-	INT			= "INT"		//12345
-	
+	IDENT = "IDENT" //add, foobar, x, y...
+	INT   = "INT"   //12345
+
 	//Operadores
-	ASSIGN		= "="
-	PLUS		= "+"
-	MINUS 		= "-"
-	BANG 		= "!"
-	ASTERISK 	= "*"
-	SLASH 		= "/"
-	LT			= "<"
-	GT			= ">"
-	EQ			= "=="
-	NOT_EQ		= "!="
-	
-	
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
 	//Delimitadores
-	COMMA		= ","
-	SEMICOLON	= ";"
-	
-	LPAREN		= "("
-	RPAREN		= ")"
-	LBRACE		= "{"
-	RBRACE		= "}"
-	
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
 	//Palabras clave
-	FUNCTION	= "FUNCTION"
-	LET			= "LET"
-	TRUE		= "TRUE"
-	FALSE		= "FALSE"
-	IF			= "IF"
-	ELSE		= "ELSE"
-	RETURN		= "RETURN"
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+
+	// TIPOS DE DATOS
+	STRING = "STRING"
 )
 
-type Token struct{
-	Type	TokenType
-	Literal	string
+type Token struct {
+	Type    TokenType
+	Literal string
 }
