4c13a15269de1d36fdd2644180375020d52c3c4c
Tomás Serpez <tomasserpez@gmail.com>
2024-02-19 15:57:41 -0300

    Algunas funciones integradas como push, first y last para los arrays agregados...

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -2,7 +2,9 @@
 
 package evaluator
 
-import "tomlang/object"
+import (
+	"tomlang/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -11,11 +13,85 @@ var builtins = map[string]*object.Builtin{
 				return newError("Cantidad erronea de argumentos. Se obtuvo=%d, Se esperaba=1", len(args))
 			}
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
 				return newError("Argumento para 'len' no soportado, se obtuvo=%s", args[0].Type())
 			}
+		},
+	},
+	"first": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("Numero erroneo de argumentos. Se obtuvo=%d, Se esperaba=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("El argumento, primero tiene que ser un ARRAY. Se obtuvo=%s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
+		},
+	},
+	"last": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("Numero erroneo de argumentos. Se obtuvo=%d, Se esperaba=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("El argumento, primero tiene que ser un ARRAY. Se obtuvo=%s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				return arr.Elements[length-1]
+			}
+			return NULL
+		},
+	},
+	"rest": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("Numero erroneo de argumentos. Se obtuvo=%d, Se esperaba=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("El argumento, primero tiene que ser un ARRAY. Se obtuvo=%s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
+		},
+	},
+	"push": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+
+			if len(args) != 2 {
+				return newError("Numero erroneo de argumentos. Se obtuvo=%d, Se esperaba=2", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("El argumento, primero tiene que ser un ARRAY. Se obtuvo=%s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+
+			return &object.Array{Elements: newElements}
 		},
 	},
 }
